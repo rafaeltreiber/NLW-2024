7aa09c2b6c6409303c3b1dec28127aa7398b0677
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2024-04-08 19:03:57 -0300

    styling

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,4 +1,5 @@
 import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface IconButtonprops extends ComponentProps<'button'> {
   transparent?: boolean
@@ -8,10 +9,11 @@ export function IconButton({ transparent, ...props }: IconButtonprops) {
   return (
     <button
       {...props}
-      className={transparent
-        ? 'bg-black/20 border border-white/10 rounded-md p-1.5'
-        : 'bg-white/10 border border-white/10 rounded-md p-1.5'
-      }
+      className={twMerge(
+        'border border-white/10 rounded-md p-1.5',
+        transparent ? 'bg-black/20' : 'bg-white/10',
+        props.disabled ? 'opacity-50' : null
+      )}
     />
   )
 }
