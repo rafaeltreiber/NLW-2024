6e9baf9a30048a479395d09a7271d8b8da96ab78
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2024-04-08 18:59:25 -0300

    loaded with fake data

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import nlwuniteicon from '../assets/nlw-unite-icon.svg'
+import { NavLink } from './nav-link'
 
 export function Header() {
   return (
@@ -6,8 +7,9 @@ export function Header() {
       <img src={nlwuniteicon} />
 
       <nav className='flex items-center gap-5'>
-        <a href='' className='font-medium text-sm text-zinc-300'>Eventos</a>
-        <a href='' className='font-medium text-sm'>Participantes</a>
+
+        <NavLink href='/eventos'>Eventos</NavLink>
+        <NavLink href='/participantes'>Prticipantes</NavLink>
       </nav>
     </div>
   )
